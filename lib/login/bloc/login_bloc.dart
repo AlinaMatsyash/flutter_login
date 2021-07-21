4b3b9b92167c6ae978942756345e68412a913e49
AlinaMatsyash <alina.mathyahs@gmail.com>
2021-07-21 16:21:44 +0300

    (login_bloc)

---
+++ lib/login/bloc/login_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_login/authentication/bloc/authentication_bloc.dart';
+import 'package:flutter_login/login/login.dart';
+import 'package:formz/formz.dart';
+
+part 'login_event.dart';
+part 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  LoginBloc({
+    required AuthenticationRepository authenticationRepository,
+}) : _authenticationRepository = authenticationRepository,
+  super(const LoginState());
+  final AuthenticationRepository _authenticationRepository;
+
+  @override
+  Stream<LoginState> mapEventToState(
+      LoginEvent event,
+      ) async* {
+    if (event is LoginUsernameChanged) {
+      yield _mapUsernameChangedToState(event, state);
+    } else if (event is LoginPasswordChanged) {
+      yield _mapPasswordChangedToState(event, state);
+    } else if (event is LoginSubmitted) {
+      yield* _mapLoginSubmittedToState(event, state);
+    }
+  }
+
+
+
+
+}
